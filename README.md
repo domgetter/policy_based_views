956abaa8384a7dbef4767da92c352d1fdf9a71ef
Frank West <frank.west.iii@gmail.com>
2015-09-02 16:44:46 -0700

    Added clearance authentication
    Fixes #7

---
+++ README.md
@@ -0,0 +1,19 @@
+# Getting Started
+
+1. Clone
+2. bundle install
+3. rake db:migrate
+4. rake db:seed
+5. bundle exec rails s
+
+## Logins
+
+### SMB
+
+email: smb@example.com
+password: password
+
+### Enterprise
+
+email: enterprise@example.com
+password: password
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902232424) do
+ActiveRecord::Schema.define(version: 20150902233543) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -31,8 +31,14 @@ ActiveRecord::Schema.define(version: 20150902232424) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.integer  "entity_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "encrypted_password", limit: 128
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128
   end
+
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
