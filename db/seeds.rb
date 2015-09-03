40b1765cbb2b4f412b0ad081b3041ba0112d34eb
Frank West <frank.west.iii@gmail.com>
2015-09-02 17:47:32 -0700

    Adds genre to books
    Fixes #6

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ enterprise = Entity.create(classification: 'enterprise', name: 'Evil Corp', logo
 
 User.create(email: 'smb@example.com', entity: smb, password: 'password')
 User.create(email: 'enterprise@example.com', entity: enterprise, password: 'password')
+
+business = Genre.create(name: 'Business')
+fiction = Genre.create(name: 'Fiction')
+
+25.times do |i|
+  genre = [business, fiction].sample
+  Book.create(title: Faker::Book.title, author: Faker::Name.name, genre: genre)
+end
